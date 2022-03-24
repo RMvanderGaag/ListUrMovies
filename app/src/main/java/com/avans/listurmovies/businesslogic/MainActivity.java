3d8bbb4892a59770ebf23f8b6667d9e9baddc55e
Luuk Vogel <lmt.vogel@student.avans.nl>
2022-03-24 09:54:07 +0100

    Added the packages: presentation, businesslogic, dataaccess and domain.

--- app/src/main/java/com/avans/listurmovies/businesslogic/MainActivity.java
+++ app/src/main/java/com/avans/listurmovies/businesslogic/MainActivity.java
@@ -1,7 +1,8 @@
-package com.avans.listurmovies;
+package com.avans.listurmovies.businesslogic;
 
 import android.os.Bundle;
 
+import com.avans.listurmovies.R;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
