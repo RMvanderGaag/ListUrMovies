0f703079c50002f1d4b2120eaf49f92bccd10892
Rensvdk20 <rensvdk@gmail.com>
2022-03-26 16:52:51 +0100

    Added Retrofit

---
+++ app/src/main/java/com/avans/listurmovies/dataacess/RetrofitClient.java
@@ -0,0 +1,29 @@
+package com.avans.listurmovies.dataacess;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static RetrofitClient mInstance = null;
+    private MovieAPI mRepository;
+
+    public RetrofitClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        mRepository = retrofit.create(MovieAPI.class);
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if(mInstance == null) {
+            mInstance = new RetrofitClient();
+        }
+
+        return mInstance;
+    }
+
+    public MovieAPI getmRepository() {
+        return mRepository;
+    }
+}
