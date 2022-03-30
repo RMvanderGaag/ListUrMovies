3ac2c066198fd1a6c7949b60ee8a44fcbe05c40f
42837230+RMvanderGaag@users.noreply.github.com <42837230+RMvanderGaag@users.noreply.github.com>
2022-03-30 13:11:53 +0200

    Started with search function

--- app/src/main/java/com/avans/listurmovies/domain/Movie.java
+++ app/src/main/java/com/avans/listurmovies/domain/Movie.java
@@ -3,10 +3,11 @@ package com.avans.listurmovies.domain;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity(tableName = "movies")
-public class Movie {
+public class Movie implements Serializable {
     //General
     @PrimaryKey
     private int id;
